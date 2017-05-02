82dd355820ed8832d40a886eb6826796305c2586
SmallpTsaiMTK <smallp.tsai@mediatek.com>
2017-05-02 15:48:28 +0800

    mt76x7_hdk -> linkit7697_hdk

--- project/linkit7697_hdk/apps/ble_smart_connect/GCC/feature.mk
+++ project/linkit7697_hdk/apps/ble_smart_connect/GCC/feature.mk
@@ -1,5 +1,5 @@
 IC_CONFIG                           = mt7697
-BOARD_CONFIG                        = mt7697_hdk
+BOARD_CONFIG                        = linkit7697_hdk
 MTK_FW_VERSION                      = mt7697_fw
 MTK_DEBUG_LEVEL                     = info
 MTK_HCI_CONSOLE_MIX_ENABLE          = y
